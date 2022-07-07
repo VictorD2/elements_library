0d3e273472b8d8b2c83f21c0137145fe3d695c34
Victor Hernandez <victor-2027@hotmail.com>
2022-07-06 19:11:09 -0500

    perf: :zap: adding width and height props to container element

--- src/elements/app_container/app_container.tsx
+++ src/elements/app_container/app_container.tsx
@@ -6,15 +6,17 @@ type AppContainerProps = {
   justifyContent?: string;
   alignItems?: string;
   className?: string;
+  width?: string;
+  height?: string;
 };
 
-export const AppContainer = ({ className, display = "flex", bgColor = "bg-white", children, flexDirection = "flex-row" }: AppContainerProps) => {
+export const AppContainer = ({ className, width = "w-full", height = "h-10", display = "flex", bgColor = "bg-white", children, flexDirection = "flex-row" }: AppContainerProps) => {
   const classNames = (...classes: any): string => {
     return classes.filter(Boolean).join(" ");
   };
 
   const setClassNames = (): string => {
-    return classNames(display, bgColor, flexDirection, className);
+    return classNames(display, bgColor, flexDirection, className, width, height);
   };
 
   return <div className={setClassNames()}>{children}</div>;
